89988c0f6a1d04ecae7fcc5a6c977078686944d1
Mathias <email@example.com>
2024-01-19 16:42:07 +0100

    finish for today

--- src/start-chrome.sh
+++ src/start-chrome.sh
@@ -1,5 +1,6 @@
-export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1001/bus
+export DISPLAY=:1
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 xhost +
 # google-chrome-stable --no-sandbox --no-gpu --use-gl=desktop -ozone-plattform=wayland --disable-dev-shm-usage
 # --disable-gpu --disable-software-rasterizer
-google-chrome-stable --no-sandbox --no-gpu --use-gl=desktop -ozone-plattform=wayland --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer
+google-chrome-stable --no-sandbox --no-gpu --use-gl=desktop -ozone-plattform=wayland --disable-dev-shm-usage --disable-gpu --disable-software-rasterizer --no-init
